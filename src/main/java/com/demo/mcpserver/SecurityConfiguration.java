eaaccbfd3d1537075369a6ae2ce43903598cc7a3
Yanming Zhou <zhouyanming@gmail.com>
2025-07-30 09:47:18 +0800

    Use stable JWKSource

--- src/main/java/com/demo/mcpserver/SecurityConfiguration.java
+++ src/main/java/com/demo/mcpserver/SecurityConfiguration.java
@@ -1,5 +1,12 @@
 package com.demo.mcpserver;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,10 +15,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 import static org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer.authorizationServer;
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(SecurityConfiguration.RsaKeyProperties.class)
 class SecurityConfiguration {
 
 	@Bean
@@ -24,4 +35,18 @@ class SecurityConfiguration {
 			.build();
 	}
 
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(RsaKeyProperties properties) {
+		RSAKey rsaKey = new RSAKey.Builder(properties.publicKey())
+				.privateKey(properties.privateKey())
+				.keyID("secured-key-id")
+				.build();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	@ConfigurationProperties("rsa-keys")
+	public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+
+	}
 }
