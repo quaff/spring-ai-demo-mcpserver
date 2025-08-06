81c54697838e6d5c66e3ea308a42ae41461c6c68
Yanming Zhou <zhouyanming@gmail.com>
2025-08-06 11:07:50 +0800

    Supports Authorization Code Flow

--- src/main/java/com/demo/mcpserver/SecurityConfiguration.java
+++ src/main/java/com/demo/mcpserver/SecurityConfiguration.java
@@ -9,47 +9,100 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-
-import static org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer.authorizationServer;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableConfigurationProperties(SecurityConfiguration.RsaKeyProperties.class)
 class SecurityConfiguration {
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		DefaultBearerTokenResolver resolver = new DefaultBearerTokenResolver();
-		resolver.setAllowUriQueryParameter(true);
-		return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-			.with(authorizationServer(), Customizer.withDefaults())
-			.oauth2ResourceServer(resource -> resource.jwt(Customizer.withDefaults()).bearerTokenResolver(resolver))
-			.csrf(CsrfConfigurer::disable)
-			.cors(Customizer.withDefaults())
-			.build();
-	}
+    @Bean
+    @Order(1)
+    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                OAuth2AuthorizationServerConfigurer.authorizationServer();
 
-	@Bean
-	public JWKSource<SecurityContext> jwkSource(RsaKeyProperties properties) {
-		RSAKey rsaKey = new RSAKey.Builder(properties.publicKey())
-				.privateKey(properties.privateKey())
-				.keyID("secured-key-id")
-				.build();
-		JWKSet jwkSet = new JWKSet(rsaKey);
-		return new ImmutableJWKSet<>(jwkSet);
-	}
+        http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                )
+                .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults());
+        return http.build();
+    }
 
-	@ConfigurationProperties("rsa-keys")
-	public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(resource -> resource.jwt(Customizer.withDefaults()))
+                .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
 
-	}
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RsaKeyProperties properties) {
+        RSAKey rsaKey = new RSAKey.Builder(properties.publicKey())
+                .privateKey(properties.privateKey())
+                .keyID("secured-key-id")
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    @ConfigurationProperties("rsa-keys")
+    public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+
+    }
 }
